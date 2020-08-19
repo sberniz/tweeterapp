bb1bc9fa4f154d5fd9f91876c7f9adc43cce5d95
Santiago Berniz <sberniz@gmail.com>
2020-08-19 16:07:16 -0700

    assignment adding data science to web app

--- tweeterapp/app.py
+++ tweeterapp/app.py
@@ -1,9 +1,9 @@
 
 """Main app/routing file for TwitOff."""
-from flask import Flask, render_template, escape
+from flask import Flask, render_template, escape, request
 from .models import DB, User
-from .twitter import insert_example_users
-
+from .twitter import insert_example_users, add_or_update_user
+from .predict import predict_user
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
@@ -29,9 +29,37 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title='Reset database!')
-    @app.route('/user/<username>')
-    def display_tweets(username):
-        return render_template('tweets.html',title="Tweets",user = User.query.filter(User.name == username).one())
+    # @app.route('/user/<username>')
+    # def display_tweets(username):
+    #     return render_template('tweets.html',title="Tweets",user = User.query.filter(User.name == username).one())
 
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+    
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
 
     return app
